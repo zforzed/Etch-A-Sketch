5ff6348934f5ffc5d2d24f00046667982db0d37d
zforzed <mi6a2006@gmail.com>
2022-09-09 18:09:17 +0300

    Add first load grid and set side size button.

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-let div = document.createElement('div');
-for (let i=0; i<16; i++) {
-   document.querySelector(".container").innerHTML += "<div class='square'></div>";
-}
-
-document.querySelectorAll(".square").forEach(item => {
-   item.addEventListener("mouseover", (event) => {
-      event.target.classList.add("square-hover");
+let gridSide = 4;
+// add first load grid 4x4 with eventListener for each square
+createGrid(gridSide);
+function createGrid(gridSide) {
+   document.querySelector(".container").innerHTML=""; 
+   let squareWidth = (Math.floor((100/gridSide)*10))/10+"%";
+   for (let i = 0; i < gridSide*gridSide; i++) {
+      document.querySelector(".container").innerHTML += "<div class='square' style='width:"+squareWidth+"'></div>";
+   }
+   document.querySelectorAll(".square").forEach(item => {
+      item.addEventListener("mouseover", (event) => {
+         event.target.classList.add("square-hover");
+      });
    });
+}
+// button with popup to set grid side size
+document.querySelector("button").addEventListener("click", (event) => {
+   gridSide = Number(prompt("Grid side size: "));
+   createGrid(gridSide);
 });
+
+
